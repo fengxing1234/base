9919d4723a51960cbd9b45eebf86481ff11407ba
fengxing <f296016140@gmail.com>
2022-06-02 14:24:41 +0800

    解决命令行编译失败问题

---
+++ app/src/main/java/com/zhyen/base/APP.java
@@ -0,0 +1,17 @@
+package com.zhyen.base;
+
+import android.app.Application;
+import android.support.multidex.MultiDex;
+
+/**
+ * author : fengxing
+ * date : 2022/6/2 下午1:54
+ * description :
+ */
+public class APP extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MultiDex.install(this);
+    }
+}
