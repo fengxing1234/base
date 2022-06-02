79f243c64ecb0065fe7ec14aef469be5a297532f
fengxing <f296016140@gmail.com>
2022-06-02 11:44:45 +0800

    组件化Gradle配置

---
+++ common/src/main/java/com/zhyen/common/InitARouter.java
@@ -0,0 +1,30 @@
+package com.zhyen.common;
+
+import android.app.Application;
+
+
+
+/**
+ * author : fengxing
+ * date : 2022/6/2 上午9:29
+ * description :
+ */
+public final class InitARouter {
+
+    public InitARouter() {
+        //isDebug = BuildConfig.DEBUG;
+
+    }
+
+    public void init(Application application) {
+        init(application, true);
+    }
+
+    public void init(Application application, boolean isDebug) {
+//        if (isDebug) {           // 这两行必须写在init之前，否则这些配置在init过程中将无效
+//            ARouter.openLog();     // 打印日志
+//            ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+//        }
+//        ARouter.init(application); // 尽可能早，推荐在Application中初始化
+    }
+}
